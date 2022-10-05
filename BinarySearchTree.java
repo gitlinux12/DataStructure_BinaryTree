cfdad739b3a7c666d13bad53d5ae019478dd51f0
gitlinux12 <mshirole94@gmail.com>
2022-10-05 22:31:30 +0530

    Add Element from Given Binary Tree

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -45,10 +45,20 @@ public class BinarySearchTree<K extends Comparable <K>> {
 	}
 	public static void main(String args[]) {
 		BinarySearchTree<Integer> binary = new BinarySearchTree<>();
-	binary.add(56);
-	binary.add(30);
-	binary.add(70);
-	
+		binary.add(56);
+		binary.add(30);
+		binary.add(70);
+		binary.add(22);
+		binary.add(40);
+		binary.add(60);
+		binary.add(95);
+		binary.add(11);
+		binary.add(3);
+		binary.add(16);
+		binary.add(65);
+		binary.add(63);
+		binary.add(67);
+		
 	binary.preOrder();
 	int bstSize = binary.getSize();
 	System.out.println("Size of binary tree is : "+bstSize);
